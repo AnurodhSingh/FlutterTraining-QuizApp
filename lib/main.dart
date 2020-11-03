4a4f6dddf736a559534a8001a46b0bab0198e2a3
Anurodh Singh <asingh@isystango.com>
2020-11-03 14:32:57 +0530

    app to demonstrate snackbar and model in flutter

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/ui/quiz_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: QuizScreen())
+  );
+}
